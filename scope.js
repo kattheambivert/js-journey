21bbd277b5a444b3bdd05e60b4236dd71e098b83
kattheambivert <marketing.katnguyen@gmail.com>
2022-08-24 00:29:31 +1200

    scope.js

---
+++ scope.js
@@ -0,0 +1,22 @@
+var x = 3;
+function letCompare() {
+    {
+        let x = 2;
+    }
+    let x = 4;
+    var y = 5;
+    return y;
+}
+let y = letCompare();
+console.log(x);
+console.log(y);
+// letCompare();
+// if not using let, var, const, a variable is automatically global
+
+const outerWear = "T-Shirt";
+function myOutfit() {
+    const outerWear = "dress"; //local variable takes precedence over the global variable
+    return outerWear;
+}
+let testingLocalPrecedence = myOutfit();
+console.log(testingLocalPrecedence);
