8929ce0098995b9b9b56da111a52c878cf7c36d9
kattheambivert <marketing.katnguyen@gmail.com>
2022-09-01 00:51:37 +1200

    else_statements.js

--- else_statements.js
+++ else_statements.js
@@ -44,3 +44,21 @@ function orderMyLogic(val) {
 console.log(orderMyLogic(4));
 console.log(orderMyLogic(6));
 console.log(orderMyLogic(11));
+
+// chaining if else statements
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
+    } else if (num >= 20) {
+        return "Huge";
+    }
+    // Only change code above this line
+}
+console.log(testSize(7));
