e68c1be0b25e65f96cf674e4790ce30f847f0e22
kattheambivert <marketing.katnguyen@gmail.com>
2022-09-01 20:12:32 +1200

    else_statements.js

--- else_statements.js
+++ else_statements.js
@@ -62,3 +62,52 @@ function testSize(num) {
     // Only change code above this line
 }
 console.log(testSize(7));
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if (strokes == 1) {
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes === par - 1) {
+        return names[2];
+    } else if (strokes === par) {
+        return names[3];
+    } else if (strokes === par + 1) {
+        return names[4];
+    } else if (strokes === par + 2) {
+        return names[5];
+    } else if (strokes >= par + 3) {
+        return names[6];
+    }
+}
+
+console.log(golfScore(5, 4));
+console.log(golfScore(5, "4"));
+console.log(golfScore("5", 4));
+console.log(golfScore("5", "4"));
+
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+            return "alpha";
+            break;
+        case 2:
+            return "beta";
+            break;
+        case 3:
+            return "gamma";
+            break;
+        case 4:
+            return "delta";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+
+console.log(caseInSwitch(1));
