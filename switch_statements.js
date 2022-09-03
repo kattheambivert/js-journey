8692ba09a1d80657c52bb03ff85dfa3c7a60977f
kattheambivert <marketing.katnguyen@gmail.com>
2022-09-04 00:53:17 +1200

    switch_statements.js

---
+++ switch_statements.js
@@ -0,0 +1,45 @@
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+            return "alpha";
+            break;
+        case 2:
+            return "beta";
+            break;
+        case 3:
+            return "gamma";
+            break;
+        case 4:
+            return "delta";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+
+console.log(caseInSwitch(1));
+
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case "a":
+            return "apple";
+            break;
+        case "b":
+            return "bird";
+            break;
+        case "c":
+            return "cat";
+            break;
+        default:
+            return "stuff";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+
+console.log(switchOfStuff(1));
